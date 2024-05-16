e820688c51bec1ae49423b63942456a3ef7138c6
vmore1981 <vmore.corp@gmail.com>
2024-05-15 21:48:18 -0500

    created a homepage page objec class

---
+++ src/test/java/pages/SauceHomepage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Utils.Driver;
+
+public class SauceHomepage {
+
+	
+	public SauceHomepage() {
+		
+		PageFactory.initElements(Driver.getDriver(), this);
+		
+	}
+	
+	
+	@FindBy (css = ".inventory_item_price")
+	public List<WebElement> itemPrices;
+	
+	
+	
+}
